c214be2bcb00c18d319a5b78d59d210829e50ba4
chaitanyamore003 <chaitanyamore003@gmail.com>
2026-06-27 18:33:29 +0530

    first project

--- 01-Spring-IOC-Container/src/main/java/in/springBoot/Main.java
+++ 01-Spring-IOC-Container/src/main/java/in/springBoot/Main.java
@@ -1,7 +1,14 @@
 package in.springBoot;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     static void main(String[] args) {
-        System.out.println("Hello World");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+        OrderFood food = context.getBean(OrderFood.class);
+        food.order();
     }
 }
