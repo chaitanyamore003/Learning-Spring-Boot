6ee63aad13104053d8d6467cd663ee8f9bbdf88d
chaitanyamore003 <chaitanyamore003@gmail.com>
2026-06-28 08:04:43 +0530

    added spring dependency injection fil

--- 01-Spring-IOC-Container/src/main/java/in/springBoot/OrderFood.java
+++ 01-Spring-IOC-Container/src/main/java/in/springBoot/OrderFood.java
@@ -1,16 +1,32 @@
 package in.springBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OrderFood {
-    private Payment payment;
 
-    public OrderFood(Payment payment){
-        this.payment = payment;
-    }
-    public void order(){
-        payment.pay();
-        System.out.println("Order Placed");
-    }
+        /**types of Dependency Injection*/
+
+        //method 1 - Field Injection
+        @Autowired
+        private Payment payment;
+
+
+      //method 2 - using Constructor //MOST RECOMMENDED
+//    @Autowired
+//    public OrderFood(Payment payment){
+//        this.payment = payment;
+//    }
+
+      //method 3 - Using Setter
+//    @Autowired
+//    public void setPayment(Payment payment) {
+//        this.payment = payment;
+//    }
+
+     public void order(){
+            payment.pay();
+            System.out.println("Order Placed");
+     }
 }
